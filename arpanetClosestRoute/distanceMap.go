fb6c874b95aa0e62001d6bd09758f1fa4f3545a2
stonkol <9.5cc5b@slmails.com>
2025-05-24 16:38:20 +0800

    update arpanet algo

--- arpanetClosestRoute/distanceMap.go
+++ arpanetClosestRoute/distanceMap.go
@@ -1,4 +1,7 @@
 // graph.go
+//
+// AMES(15, NASA), ARPA(Advanced Research Projects Agency), CMU, Harvard, Hawaii, Illinois, UCL(London, first intl' ARPANET node), MIT(6), NBS(National Bureau of Standards), NSA, NYU, Pentagon, Scott, SDCA(Seismic Data Analysis Center), Stanford, Texas, UCLA(first node), Utah, Xerox
+
 package main
 
 type Edge struct {
@@ -6,23 +9,25 @@ type Edge struct {
 	Weight int
 }
 
-// AMES(15), ARPA, CMU, Harvard, Hawaii, Illinois, London, MIT(6), NBS, NSA, NYU, Pentagon, Stanford, Texas, UCLA, Utah, Xerox
 var graph = map[string][]Edge{
-	"AMES":     {{"ARPA", 15}},
-	"ARPA":     {{"CMU", 10}, {"Harvard", 20}},
-	"CMU":      {{"Harvard", 5}, {"NSA", 12}},
-	"Harvard":  {{"NSA", 7}, {"NYU", 15}},
-	"Hawaii":   {{"Illinois", 25}},
-	"Illinois": {{"London", 30}},
-	"London":   {{"MIT", 6}},
-	"MIT":      {{"NBS", 8}},
-	"NBS":      {{"NSA", 3}},
-	"NSA":      {{"NYU", 4}},
-	"NYU":      {{"Pentagon", 10}},
-	"Pentagon": {{"Stanford", 18}},
-	"Stanford": {{"Texas", 20}, {"UCLA", 15}},
-	"Texas":    {{"UCLA", 10}},
-	"UCLA":     {{"Utah", 12}, {"Xerox", 15}},
+	"AMES":     {{"Hawaii", 3}, {"Stanford", 7}, {"UCLA", 15}, {"Utah", 12}, {"Xerox", 8}},
+	"ARPA":     {{"SDAC", 2}, {"Pentagon", 5}, {"Texas", 14}},
+	"CMU":      {{"SDCA", 15}, {"Scott", 7}, {"Lincoln", 6}},
+	"Harvard":  {{"Scott", 12}, {"MIT", 14}, {"NYU", 1}, {"Lincoln", 9}},
+	"Hawaii":   {},
+	"Illinois": {{"MIT", 10}, {"Utah", 3}},
+	"Lincoln":  {{"MIT", 7}},
+	"MIT":      {},
+	"NBS":      {{"NYU", 4}, {"NSA", 3}, {"Pentagon", 4}},
+	"NSA":      {{"NYU", 4}, {"SDAC", 3}},
+	"NYU":      {{"SCAC", 10}},
+	"Pentagon": {{"Texas", 14}},
+	"Scott":    {{"Stanford", 16}, {"UCLA", 11}, {"Xerox", 13}},
+	"SDAC":     {{"UCL", 7}, {"Texas", 8}},
+	"Stanford": {{"Texas", 22}, {"UCLA", 11}, {"Xerox", 9}},
+	"Texas":    {{"UCLA", 20}},
+	"UCL":      {},
+	"UCLA":     {{"Xerox", 15}},
 	"Utah":     {{"Xerox", 7}},
 	"Xerox":    {},
 }
